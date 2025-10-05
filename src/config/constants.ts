387ae9506025a5f7a30800897128c53d482ba3a9
ahmedaliv <ahmed195131@feng.bu.edu.eg>
2025-10-05 07:34:06 +0300

    feat: add constants file for reconciliation paths

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const SOURCE_CSV_PATH = path.resolve('./data/source_transactions.csv');
+export const SYSTEM_CSV_PATH = path.resolve('./data/system_transactions.csv');
+export const OUTPUT_DIR = path.resolve('./output');
